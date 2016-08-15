fee319ea77cbb9e5397898bd46bfb6d15d6add33
AlexKVal <alexkval@gmail.com>
2016-08-15 21:02:50 +0300

    add api versioning and enforce json

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1 do
+      resources :comments
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
